ddef6d49cc75936bcff2d0218a13379709090e2e
IJAS AHAMMED A P <86339769+ijas0805@users.noreply.github.com>
2022-05-28 00:11:40 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import logging
 
-logging.basicConfig(filename="production.log",
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
+# logging.basicConfig(filename="production.log",
+#                     format='%(asctime)s %(message)s',
+#                     filemode='w')
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
